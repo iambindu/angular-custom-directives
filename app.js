5bb6e8e2ae7570251f8b4ef95a5c9728ccf24aab
iambindu <you@example.com>
2017-05-20 15:48:52 +0530

    complete directive

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var app=angular.module("myApp",[])
 
 app.controller("myController",["$scope",function($scope){
-$scope.message="this is controller i know"
+$scope.message="this is controller i know";
+$scope.company="TechMinds Pvt ltd"
+$scope.userDetails=[
+{name :"Scott Desatnick",address:"Boston,United States"},
+{name :"Srikanth",address:"Boston,United States"},
+{name :"Scott",address:"Boston,United States"}
+
+
+]
+$scope.change=function(){
+	$scope.company="techmind solutions";
+}
 }])
 
 app.directive("myCustomerDetailsDirective",function(){
 return {
 	restrict:'EACM',
-	template:"<h1>This is custom directive</h1>",
-	replace: true
+	templateUrl: 'customerDetails.html',
+	replace: true,
+	scope:{
+		companyName :'@',
+		userDetails :'=',
+		change:'&'
+	}
+
 }
 
 })
